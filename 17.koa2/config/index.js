6af07967d120f96cb4d8c3fbc0557906b1fd5321
ZAIJIANLUOYE110 <779793115@qq.com>
2016-12-05 18:07:11 +0800

    mysql配置

---
+++ 17.koa2/config/index.js
@@ -0,0 +1,5 @@
+import mysql from './mysql'
+
+module.exports = {
+  mysql
+}
---
+++ 17.koa2/config/mysql.js
@@ -0,0 +1,7 @@
+export default {
+  database: 'test',
+  username: 'root',
+  password: 'GENG1234',
+  host: 'localhost',
+  port: 3306 // 默认
+}
