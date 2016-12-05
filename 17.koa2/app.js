a9f16c6d9405bc34c409c8e9d82c2e8e4621b7de
coderGLM <779793115@qq.com>
2016-12-06 07:24:53 +0800

    移除数据库设置blabla

--- 17.koa2/app.js
+++ 17.koa2/app.js
@@ -1,5 +1,6 @@
 //
 // koa: http://www.liaoxuefeng.com/wiki/001434446689867b27157e896e74d51a89c25cc8b43bdb3000/001471087582981d6c0ea265bf241b59a04fa6f61d767f6000
+// sequelize: http://node-webdevelopment.blogspot.com/2012/12/async-queries-with-sequilizejs.html
 //
 
 //
@@ -20,80 +21,31 @@ const controller = require('./lib/middleware/controller')
 const templating = require('./lib/middleware/nunjucks')
 const Sequelize = require('sequelize')
 const config = require('./config')
+const model = require('./models')
 const isProduction = process.env.NODE_ENV == 'production'
 
-var sequelize = new Sequelize(config.mysql.database, config.mysql.username, config.mysql.password, {
-  host: config.mysql.host,
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 30000
-  }
-})
-
-var Pet = sequelize.define('pet', {
-  id: {
-    type: Sequelize.STRING(50),
-    primaryKey: true
-  },
-  name: Sequelize.STRING(100),
-  gender: Sequelize.BOOLEAN,
-  birth: Sequelize.STRING(10),
-  createdAt: Sequelize.BIGINT,
-  updatedAt: Sequelize.BIGINT,
-  version: Sequelize.BIGINT
-}, {
-  timestamps: false
-})
-
 var now = new Date()
+model.pet.create({
+  id: `g-${now.getTime()}`,
+  name: 'Gaffey',
+  gender: false,
+  birth: '2007-07-07',
+  createdAt: now.getTime(),
+  updatedAt: now.getTime(),
+  version: 0
+}).then(function (p) {
+  console.log(`created. ${JSON.stringify(p)}`)
+}).catch(err => {
+  console.log(`failed: ${err}`)
+})
 
-//Pet.create({
-//  id: `g-${now.getTime()}`,
-//  name: 'Gaffey',
-//  gender: false,
-//  birth: '2007-07-07',
-//  createdAt: now.getTime(),
-//  updatedAt: now.getTime(),
-//  version: 0
-//}).then(function (p) {
-//  console.log(`created. ${JSON.stringify(p)}`)
-//}).catch(err => {
-//  console.log(`failed: ${err}`)
-//})
-
-/* (async () => {
-  await Pet.create({
-    id: `g-${now.getTime()}`,
-    name: 'Gaffey',
-    gender: false,
-    birth: '2007-07-07',
-    createdAt: now.getTime(),
-    updatedAt: now.getTime(),
-    version: 0
-  })
-})() */
-
-Pet.findAll({
+/*Pet.findAll({
   where: {
     name: 'Gaffey'
   }
 }).then(p => {
   console.log(p)
-})
-
-/*(async () => {
-    var pets = await Pet.findAll({
-    where: {
-      name: 'Gaffey'
-    }
-  });
-  console.log(`find ${pets.length} pets:`)
-  for (let p of pets) {
-    console.log(JSON.stringify(p))
-  }})()*/
-
+})*/
 
 app.use(async (ctx, next) => {
   console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
