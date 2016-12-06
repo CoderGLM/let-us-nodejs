a717203e5bfa00bf73be8c54d384fe90cf403b58
ZAIJIANLUOYE110 <779793115@qq.com>
2016-12-06 09:15:41 +0800

    修改字段

--- 17.koa2/models/pet.js
+++ 17.koa2/models/pet.js
@@ -1,11 +1,14 @@
 import db from '../db'
 
 module.exports = db.defineModel('pets', {
-  email: {
-    type: db.STRING(100),
-    unique: true
+  id: {
+    type: db.STRING(50),
+    primaryKey: true
   },
-  passwd: db.STRING(100),
   name: db.STRING(100),
-  gender: db.BOOLEAN
+  gender: db.BOOLEAN,
+  birth: db.STRING(10),
+  createdAt: db.BIGINT,
+  updatedAt: db.BIGINT,
+  version: db.BIGINT
 })
